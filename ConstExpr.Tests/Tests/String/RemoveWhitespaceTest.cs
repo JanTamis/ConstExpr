751270ca8f1adcbcd17cccfa63ff4ee3f592e8db
jantamiskossen <jantamiskossen@gmail.com>
2026-05-07 21:27:29 +0200

    test(tests): add unit test for RemoveWhitespace function in string operations

---
+++ ConstExpr.Tests/Tests/String/RemoveWhitespaceTest.cs
@@ -0,0 +1,33 @@
+using ConstExpr.Core.Enumerators;
+
+namespace ConstExpr.Tests.String;
+
+[InheritsTests]
+public class RemoveWhitespaceTest() : BaseTest<Func<string, string>>(FastMathFlags.FastMath)
+{
+	public override string TestMethod => GetString(input =>
+	{
+		if (System.String.IsNullOrEmpty(input))
+			return input;
+
+		var result = new char[input.Length];
+		var index = 0;
+
+		foreach (var c in input)
+		{
+			if (!Char.IsWhiteSpace(c))
+				result[index++] = c;
+		}
+
+		return new string(result, 0, index);
+	});
+
+	public override IEnumerable<KeyValuePair<string?, object?[]>> TestCases =>
+	[
+		Create(null),
+		Create("return \"HelloWorld\";", "Hello World"),
+		Create("return \"TestString\";", "  Test  String  "),
+		Create("return \"\";", "   "),
+		Create("return \"abc\";", "abc")
+	];
+}
